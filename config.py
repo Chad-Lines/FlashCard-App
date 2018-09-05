c30975b045d41448347583fe46481819719875c6
Chad Lines <chadlines@outlook.com>
2018-09-04 23:21:48 -0600

    Start from scratch

--- config.py
+++
@@ -1,7 +0,0 @@
-
-
-class Configure(object):
-    DEBUG = True
-    SECRET_KEY = 'Passw0rd'
-    SQLALCHEMY_DATABASE_URI = 'postgres://chad:Passw0rd@localhost:5432/flashcard'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
