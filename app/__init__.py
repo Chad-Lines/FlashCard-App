481beabbf60db5b6c769bcaa3727e34d2cd3088b
Chad Lines <chadlines@outlook.com>
2018-09-10 15:39:31 -0600

    (NW)  Card Good/Bad Functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
